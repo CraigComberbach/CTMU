f4ea7559cd92b34b9f1739d66a3a0266be8496b3
CraigComberbach <craig.comberbach@gmail.com>
2015-05-21 13:42:19 -0500

    Works
    On reference board

--- CTMU.c
+++ CTMU.c
@@ -1,5 +1,6 @@
+//Document initialize functionettes
 /**************************************************************************************************
-Target Hardware:		PIC24FJ256GA1xx
+Target Hardware:		PIC24Fxxxx
 Code assumptions:
 Purpose:
 Notes:
@@ -38,22 +39,22 @@ vnext	Y-M-D	Craig Comberbach	Compiler: C30 v3.31	Optimization: 0	IDE: MPLABx 1.9
 void CTMU_Initialize(void)
 {
 	//CTMUCON
-	CTMUCONbits.CTMUEN =  0; //make sure CTMU is disabled
-	CTMUCONbits.CTMUSIDL = 0; //CTMU continues to run in idle mode
-	CTMUCONbits.TGEN =  0; //disable edge delay generation mode of the CTMU
-	CTMUCONbits.EDGEN =  0; //edges are blocked
-	CTMUCONbits.EDGSEQEN = 0; //edge sequence not needed
-	CTMUCONbits.IDISSEN =  0; //Do not ground the current source
-	CTMUCONbits.CTTRIG =  0; //Trigger Output is disabled
-	CTMUCONbits.EDG2POL =  0;
-	CTMUCONbits.EDG2SEL =  0x3; //Edge2 Src = OC1 (don?t care)
-	CTMUCONbits.EDG1POL =  1;
-	CTMUCONbits.EDG1SEL =  0x3; //Edge1 Src = Timer1 (don?t care)
+	CTMUCONbits.CTMUEN		= 0;
+	CTMUCONbits.CTMUSIDL	= 0;
+	CTMUCONbits.TGEN		= 0;
+	CTMUCONbits.EDGEN		= 0;
+	CTMUCONbits.EDGSEQEN	= 0;
+	CTMUCONbits.IDISSEN		= 0;
+	CTMUCONbits.CTTRIG		= 0;
+	CTMUCONbits.EDG2POL		= 0;
+	CTMUCONbits.EDG2SEL		= 0;
+	CTMUCONbits.EDG1POL		= 0;
+	CTMUCONbits.EDG1SEL		= 0;
 
 	//CTMUICON
-	CTMUICONbits.IRNG = 3; //55uA
-	CTMUICONbits.ITRIM = 0; //Nominal - No Adjustment
-	CTMUCONbits.CTMUEN = 1; //Enable CTMU
+	CTMUICONbits.IRNG		= 3; //55uA
+	CTMUICONbits.ITRIM		= 0; //Nominal - No Adjustment
+	CTMUCONbits.CTMUEN		= 1; //Enable CTMU
 
 	return;
 }
@@ -61,46 +62,64 @@ void CTMU_Initialize(void)
 void CTMU_Start(int channel)
 {
 	int loop;
-	int loop2;
-	//Set the pin to Analog
-	Pin_Set_TRIS(PIN_PRESENCE_SENSOR, INPUT);
-	AD1CON1bits.ADON = 0;
-	AD1PCFG &= ~(1 << CAPACITIVE_PRESENSE_SENSOR);
-	AD1CON1bits.ADON = 1;
-	Nop();	Nop();	Nop();	Nop();
-	Nop();	Nop();	Nop();	Nop();
-	Nop();	Nop();	Nop();	Nop();
-	Nop();	Nop();	Nop();	Nop();
 
+	AD1CON1bits.ADON = 0;
+	AD1CON3bits.SAMC = 0b00001;
+	AD1CON1bits.ADON = 1;
+
+	//1.
+//	AD1PCFGLbits.PCFG3 = ~0;
+//	AD1PCFGL |= 1 << channel;
+//	LATBbits.LATB3 = 0;
+//	TRISBbits.TRISB3 = 0;
+//	for (loop = 0; loop < 100; loop++) //Delay for CTMU charge time
+		Nop();
+
+	//2.
+//	AD1PCFGLbits.PCFG3 = 0;
+//	AD1PCFGL &= ~(1 << channel);
+//	TRISBbits.TRISB3 = ~0;
+	AD1CHS = channel;
+
+	//3.
+	AD1CSSL |= 1 << channel;
+
+	//4.
 	CTMUCONbits.IDISSEN = 1;
 
-	Nop();	Nop();	Nop();	Nop();
-	Nop();	Nop();	Nop();	Nop();
+	//5.
+	Nop();	Nop();	Nop();	Nop();	Nop();
 
+	//6.
 	CTMUCONbits.IDISSEN = 0;
 
+//	//7.
+	AD1CON1bits.SAMP = 1;
+
+	//8.
 	CTMUCONbits.EDG2STAT = 0; // Make sure edge2 is 0
 	CTMUCONbits.EDG1STAT = 1; // Set edge1 - Start Charge
 
-//	for (loop = 0; loop < 11988; loop++) // Delay for CTMU charge time
-	for (loop2 = 0; loop2 < 40; loop2++) // Delay for CTMU charge time
-	{
-		for (loop = 0; loop < 20000; loop++) // Delay for CTMU charge time
-			asm volatile("nop");
-		ClrWdt();
-	}
+	//9.
+	for (loop = 0; loop < 10; loop++) //Delay for CTMU charge time
+		Nop();
+
+	//10.
 	CTMUCONbits.EDG1STAT = 0; //Clear edge1 - Stop Charge
+	AD1CON1bits.SAMP = 0;
+
+	//11.
+
 	return;
 }
 
 void CTMU_Stop(int channel)
 {
-	//Set the pin to digital
-	AD1CON1bits.ADON = 0;
-	AD1PCFG |= (1 << CAPACITIVE_PRESENSE_SENSOR);
-	AD1CON1bits.ADON = 1;
-	Pin_Low(PIN_PRESENCE_SENSOR);
-	Pin_Set_TRIS(PIN_PRESENCE_SENSOR, OUTPUT);
+//	//Set the pin to digital
+////	AD1PCFGL = 0xFFFF;
+//	AD1PCFGL |= (1 << CAPACITIVE_PRESENSE_SENSOR);
+//	Pin_Low(PIN_PRESENCE_SENSOR);
+//	Pin_Set_TRIS(PIN_PRESENCE_SENSOR, OUTPUT);
 
 	return;
 }
